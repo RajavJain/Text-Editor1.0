02690ab871b3f831f88f2956dfa82b1133b286ec
RajavJain <rajavjain2002@gmail.com>
2022-10-23 09:02:44 +0530

    Final Commit

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function alert(props) {
-    
+
     const capitalize = (word) => {
         const lower = word.toLowerCase();
         return lower.charAt(0).toUpperCase() + lower.slice(1);
@@ -9,7 +9,11 @@ function alert(props) {
     return (
         <div style={{ height: "50px" }}>
             {
-                props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+                //isme props.alert send kiya hai agar null hota hai then kuch nhi hoga but agar null nhi hota then jo agla div hai wo follow ho jaaegi....
+                //this syntax is commonly used by Community
+                props.alert 
+                &&
+                <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                     <strong> {capitalize(props.alert.type)} </strong>: {props.alert.msg}
                 </div>
             }
